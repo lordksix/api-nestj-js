6ad777d9a4efa67f4514908fcdaa4d95a2a6441a
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-09-13 02:17:38 -0500

    Initial project setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URI)],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot({ limit: 10, ttl: 60 }),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
